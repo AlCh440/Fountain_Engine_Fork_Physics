9d08ffa6c1187f4e514aa303bf55b6eb48ff13b6
AlCh440 <albert.chica@estudiantat.upc.edu>
2023-01-10 19:53:45 +0100

    Added a lot of things in phys module
    -sphere technically is added (comprovar)
    - debug draw is now working, even though i couldnt make sure because of scene

--- Separaty_Engine/Game/Source/ModulePhysics.cpp
+++ Separaty_Engine/Game/Source/ModulePhysics.cpp
@@ -22,36 +22,42 @@ ModulePhysics::ModulePhysics() : Module()
 	broad_phase = new btDbvtBroadphase();
 	solver = new btSequentialImpulseConstraintSolver();
 	
+	debug_draw = new DebugDrawer();
 }
 
 // Destructor
 ModulePhysics::~ModulePhysics()
 {
+	delete debug_draw;
 	delete solver;
 	delete broad_phase;
 	delete dispatcher;
 	delete collision_conf;
 }
 
-// Render not available yet----------------------------------
-bool ModulePhysics::Init()
-{
-	
 
-	return true;
-}
 
 // ---------------------------------------------------------
 bool ModulePhysics::Start()
 {
-	
+	world = new btDiscreteDynamicsWorld(dispatcher, broad_phase, solver, collision_conf);
+	world->setGravity(btVector3(0, -10, 0));
+
+	world->setDebugDrawer(debug_draw);
+
+	// LETS TRY SETTING SOME GROUND
+	btMotionState* motion = new btDefaultMotionState();
+	btCollisionShape* shape = new btStaticPlaneShape(btVector3(0, 1, 0), 0);
+	btRigidBody* ground = new btRigidBody(0, motion, shape);
+	world->addRigidBody(ground);
+
 	return true;
 }
 
 // ---------------------------------------------------------
 update_status ModulePhysics::PreUpdate(float dt)
 {
-	
+	world->stepSimulation(dt, 15);
 
 	return UPDATE_CONTINUE;
 }
@@ -59,7 +65,14 @@ update_status ModulePhysics::PreUpdate(float dt)
 // ---------------------------------------------------------
 update_status ModulePhysics::Update(float dt)
 {
-	
+	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
+	{
+		Prim_Sphere s(1);
+		s.SetPos(App->camera->Position.x, App->camera->Position.y, App->camera->Position.z);
+		float force = 30.0f;
+		AddSphere(s, 1);
+	}
+
 	return UPDATE_CONTINUE;
 }
 
@@ -79,7 +92,58 @@ bool ModulePhysics::CleanUp()
 	return true;
 }
 
+btRigidBody* ModulePhysics::AddSphere(const Prim_Sphere& sphere, float mass)
+{
+	btCollisionShape* colShape = new btSphereShape(sphere.radius);
+
+
+	btTransform startTransform;
+	startTransform.setFromOpenGLMatrix(&sphere.transform);
+
+	btVector3 localInertia(0, 0, 0);
+	if (mass != 0.f)
+		colShape->calculateLocalInertia(mass, localInertia);
+
+	btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+	
+	btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+
+	btRigidBody* body = new btRigidBody(rbInfo);
+	
+
+	bodies.push_back(body);
+	world->addRigidBody(body);
+	
+
+	return body;
+}
 
 
 
+// ----------------------------------------------------------------------
+void DebugDrawer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
+{
 
+	line.origin.Set(from.getX(), from.getY(), from.getZ());
+	line.destination.Set(to.getX(), to.getY(), to.getZ());
+	line.color.Set(color.getX(), color.getY(), color.getZ());
+	line.Render();
+}
+
+void DebugDrawer::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color)
+{
+
+	point.transform.translate(PointOnB.getX(), PointOnB.getY(), PointOnB.getZ());
+	point.color.Set(color.getX(), color.getY(), color.getZ());
+	point.Render();
+}
+
+void DebugDrawer::setDebugMode(int debugMode)
+{
+	mode = (DebugDrawModes)debugMode;
+}
+
+int	 DebugDrawer::getDebugMode() const
+{
+	return mode;
+}
